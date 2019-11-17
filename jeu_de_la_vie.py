a92094a4a1589873c0bee6c92fbf6aec15a86e52
ludo <ludovict40@gmail.com>
2019-11-17 21:04:57 +0100

    modif class optimisation

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -1,156 +1,139 @@
 import pygame
-import time
+import random
 
-class Map(pygame.sprite.Sprite):
-    #constructeur
-    def __init__(self, hauteur, largeur, screen):
-        pygame.sprite.Sprite.__init__(self)
-        self.hauteur = hauteur
-        self.largeur = largeur
-        self.image = pygame.Surface((self.largeur, self.hauteur))
-        self.rect = self.image.get_rect()
+WHITE = (255, 255, 255)
+NOIR = (0, 0, 0)
+
+class GameOfLife():
+    def __init__(self,ligne,colonne,screen):
+        self.ligne = ligne
+        self.colonne = colonne
+        self.screen = screen
         self.x = 0
         self.y = 0
-        self.screen = screen
-        self.map = []
-        self.ligne = self.largeur
-        self.colonne = self.hauteur
-        self.i = 0
-        self.j = 0
+        self.taille = 10
+        self.map = self.setup()
 
+
+    def setup(self):
+        map = []
         for i in range(self.ligne+1):#double boucle pour crée une brique dans la class mur
-            self.map.append([])
+            map.append([])
             for j in range(self.colonne+1):
-                self.cellule = [pygame.Rect(self.x, self.y, 15, 15),0]
-                self.map[i].append(self.cellule)
-                self.x = self.x + 16
-            self.y = self.y + 16
-            self.x = 0
-    
-    def update(self):
-        self.mettre_cellule()
-        keystate = pygame.key.get_pressed()
+                cellule = random.randint(0,1)
+                map[i].append(cellule)
 
-        if keystate[pygame.K_SPACE]:
-            self.verif() 
+        return map
+
+    def nouvelle_map(self):
+        nouvelle_map = []
+        for i in range(self.ligne+1):
+            nouvelle_map.append([])
+            for j in range(self.colonne):
+                cellule = 0
+                nouvelle_map[i].append(cellule)
+        return nouvelle_map
+
+    def get_cellule(self,i,j):
+        if self.map[i][j] == 1:
+            return 1
+        else:
+            return 0
+
+    def count(self,i,j):
+        compte = 0
+        compte += self.get_cellule(i+1,j+1)
+        compte += self.get_cellule(i-1,j-1)
+        compte += self.get_cellule(i+1,j-1)
+        compte += self.get_cellule(i-1,j+1)
+        compte += self.get_cellule(i+1,j)
+        compte += self.get_cellule(i-1,j)            
+        compte += self.get_cellule(i,j+1)
+        compte += self.get_cellule(i,j-1)
+
+        if self.map[i][j] == 1:
+            if compte < 2 or compte > 3:
+                return 0
+            if compte == 3 or compte == 2:
+                return 1
+        elif self.map[i][j] == 0:
+            if compte == 3:
+                return 1
+        return self.map[i][j]
+
+
+    def mise_a_jour(self):
+        nouvelle_map = self.nouvelle_map()
+        for i in range(self.ligne):
+            for j in range(self.colonne):
+                nouvelle_cellule = self.count(i,j)
+                nouvelle_map[i][j] = nouvelle_cellule
+                self.map[i][j] = nouvelle_map[i][j]
 
     def draw(self):
+        self.x = 0
+        self.y = 0
+        self.taille = 10
         for i in range(self.ligne):
             for j in range(self.colonne):
-                if self.map[i][j][1] == 1:
-                    pygame.draw.rect(self.screen, NOIR, self.map[i][j][0])
+                if self.map[i][j] == 1:
+                    pygame.draw.rect(self.screen, NOIR, (self.x,self.y,self.taille,self.taille))
+                    self.x = self.x + 11
                 else:
-                    pygame.draw.rect(self.screen, WHITE, self.map[i][j][0])
-    
-    def mettre_cellule(self):
-        for i in range(self.ligne):
-            for j in range(self.colonne):
-                if pygame.mouse.get_pressed()[0] and self.map[i][j][0].collidepoint(pygame.mouse.get_pos()):
-                    self.map[i][j][1] = 1
-    
-    def count(self,array,i,j):
-        compte = 0
-        #|
-        #| 1
-        #|  #
-        if array[i+1][j+1][1] == 1:
-            compte += 1
-        #|#
-        #| 1
-        #|  
-        if array[i-1][j-1][1] == 1:
-            compte += 1
-        #|
-        #| 1
-        #|# 
-        if array[i+1][j-1][1] == 1:
-            compte += 1
-        #|  #
-        #| 1
-        #|  
-        if array[i-1][j+1][1] == 1:
-            compte += 1
-        #|  
-        #| 1
-        #| # 
-        if array[i+1][j][1] == 1:
-            compte += 1
-        #| # 
-        #| 1
-        #|  
-        if array[i-1][j][1] == 1:
-            compte += 1
-        #|  
-        #| 1#
-        #|  
-        if array[i][j+1][1] == 1:
-            compte += 1
-        #|  
-        #|#1
-        #|  
-        if array[i][j-1][1] == 1:
-            compte += 1
-
-        return compte
-
-    
-    def verif(self):
-        for i in range(self.ligne):
-            for j in range(self.colonne):
-                if self.map[i][j][1] == 0 and self.count(self.map,i,j) == 3 or self.map[i][j][1] == 1 and (self.count(self.map,i,j) == 3 or self.count(self.map,i,j) == 2):
-                    self.map[i][j][1] = 1
-                if self.map[i][j][1] == 1 and (self.count(self.map,i,j) > 3 or self.count(self.map,i,j) < 2):
-                    self.map[i][j][1] = 0
-
-if __name__ == "__main__":
-
-    #constant taille ecran et vitesse de boucle
-    LARGEUR = 640
-    HAUTEUR = 480
-    FPS = 60
-
-    # couleur en rgb
-    WHITE = (255, 255, 255)
-    NOIR = (0, 0, 0)
-
-    # initialisation de pygame et création de la fenêtre
-    pygame.init()
-    screen = pygame.display.set_mode((LARGEUR, HAUTEUR))
-
-    pygame.display.set_caption("Game of life")#nom de la fenêtre
-    clock = pygame.time.Clock()
-    #police = pygame.font.SysFont("comicsansms", 30, 1)
-
-    #création des objets
-
-    map = Map(HAUTEUR, LARGEUR, screen)
-
-    #ajout des sprite à la liste
+                    pygame.draw.rect(self.screen, WHITE, (self.x,self.y,self.taille,self.taille))
+                    self.x = self.x + 11
+            self.y = self.y + 11
+            self.x = 0
 
 
-    # Boucle de jeu
-    launched = True
+def main():
+    if __name__ == "__main__":
 
-    while launched:
+        #constant taille ecran et vitesse de boucle
+        LARGEUR = 640
+        HAUTEUR = 640
+        FPS = 10
+        ligne = 50
+        colonne = 50
 
-        # vitesse de boucle (FPS)
-        clock.tick(FPS)
+        # couleur en rgb
 
-        # récupération d'un event
-        for event in pygame.event.get():
 
-            if event.type == pygame.QUIT:
-                launched = False
+        # initialisation de pygame et création de la fenêtre
+        pygame.init()
+        screen = pygame.display.set_mode((LARGEUR, HAUTEUR))
 
-        
-        #Update
-        map.update()
-        #Draw / rendue
+        pygame.display.set_caption("Game of life")#nom de la fenêtre
+        clock = pygame.time.Clock()
+        #police = pygame.font.SysFont("comicsansms", 30, 1)
 
-        screen.fill(NOIR)
-        map.draw()
+        #création des objets
+        game = GameOfLife(ligne,colonne,screen)
+        #ajout des sprite à la liste
 
-        # après avoir tout déssiner
-        pygame.display.flip()
+        # Boucle de jeu
+        launched = True
 
-pygame.quit()
+        while launched:
+
+            # vitesse de boucle (FPS)
+            clock.tick(FPS)
+
+            # récupération d'un event
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    launched = False
+
+            
+            #Update
+            game.mise_a_jour()
+            screen.fill(NOIR)
+            game.draw()
+
+            # après avoir tout déssiner
+            pygame.display.flip()
+
+    pygame.quit()
+
+main()
